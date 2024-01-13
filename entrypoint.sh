fd4d692d3372db2239bc2c5cffac41bbd9dc08a1
Marcos Santos <marcosv.santos@gruposbf.com.br>
2024-01-13 18:08:58 -0300

    chore(project): add entrypoint and update gitignore

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,4 @@
-echo "Testing if Postgres is accepting connections. {$PGHOST} {$PGPORT} ${PGUSER}"
-while ! pg_isready -q -h $PGHOST -p $PGPORT -U $PGUSER
-do
-  echo "$(date) - waiting for database to start"
-  sleep 2
-done
-
-# Create, migrate, and seed database if it doesn't exist.
-if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]; then
-  echo "Database $PGDATABASE does not exist. Creating..."
-  mix ecto.create
-  mix ecto.migrate
-  mix run priv/repo/seeds.exs
-  echo "Database $PGDATABASE created."
-fi
-
+mix ecto.create
+mix ecto.migrate
+mix run priv/repo/seeds.exs
 exec mix phx.server
