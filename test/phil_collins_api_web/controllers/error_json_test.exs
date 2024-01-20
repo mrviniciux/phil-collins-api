1c3891c66f33ed3516df8c911d87b9fb487f4b9b
Marcos Santos <marcosv.santos@gruposbf.com.br>
2024-01-20 12:53:08 -0300

    feat(linters): added credo and mix format

--- test/phil_collins_api_web/controllers/error_json_test.exs
+++ test/phil_collins_api_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule PhilCollinsApiWeb.ErrorJSONTest do
   use PhilCollinsApiWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert PhilCollinsApiWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert PhilCollinsApiWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
